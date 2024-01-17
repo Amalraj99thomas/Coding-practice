99cb930e169a3cc7e5eed724d0a56a02d2aee44e
Amal Thomas <reachthomas98@gmail.com>
2024-01-17 14:15:25 -0500

    grouping-anagrams

---
+++ grouping-anagrams.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+from typing import List
+class Solution(object):
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams_dict = defaultdict(list)
+
+        for word in strs:
+            count = [0] * 26
+
+            for c in word:
+                count[ord(c) - ord('a')] +=1
+
+            key = tuple(count)
+            anagrams_dict[key].append(word) #defaultdict checks if key exists
+
+        return anagrams_dict.values()
+    
+# Test input
+#strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+strs = ["a"]
+
+# Create an instance of the Solution class
+solution_instance = Solution()
+
+# Call the groupAnagrams method with the test input
+result = solution_instance.groupAnagrams(strs)
+
+# Print the result
+print(result)
+
