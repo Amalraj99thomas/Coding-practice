cce0b7306e17a86579779b0cb30d5e1f7bf7ea1d
Amal Thomas <reachthomas98@gmail.com>
2024-01-17 15:12:18 -0500

    binary_search

---
+++ binary-search.py
@@ -0,0 +1,33 @@
+from typing import List
+class Solution(object):
+    def search(self, nums: List[int], target: int) -> int:
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        #define pointers
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            m = l + ((r - l) // 2)
+            
+            if nums[m] > target:
+                r = m - 1
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                return m
+        return -1
+    
+
+#Test:
+    
+nums = [-1,0,3,4,4,5,9,12] 
+target = 9
+
+sol_ins = Solution()
+
+result = sol_ins.search(nums, target)
+
+print(result)
